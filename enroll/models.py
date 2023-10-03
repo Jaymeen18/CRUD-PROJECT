9346ed5591e97f953821201961f04d2d8b87c378
Jaymeen18 <jaymeen.bagthariya@agileinfoways.com>
2023-10-03 13:04:09 +0530

    Revert "Change title"
    This reverts commit db2829b75510f1612444c4ff5c01a77345561ddc.

--- enroll/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from datetime import date
-# Create your models here.
-class User(models.Model):
-    name=models.CharField(max_length=70)
-    email=models.EmailField(max_length=100)
-    password=models.CharField(max_length=100)
-    current_date = models.DateTimeField(auto_now_add=True)
